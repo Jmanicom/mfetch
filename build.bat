3d51134f90bf508e4d85ce171ba2cb5f04fe8f1e
ymir <joshmanicom@gmail.com>
2026-01-14 11:26:55 -0800

    Modified to create bin folder if doesn't exist

--- build.bat
+++ build.bat
@@ -1,5 +1,19 @@
 @echo off
+
+REM check if bin folder exists, if not create it
+if not exist "%USERPROFILE%\bin\" (
+    echo Creating bin folder...
+    mkdir "%USERPROFILE%\bin"
+
+    REM Add to PATH permanently (User PATH)
+    echo Adding bin foler to PATH
+    setx PATH "%PATH%;%USERPROFILE%\bin"
+    echo NOTE: Restart terminal for PATH change to take effect
+)
+
+REM compile
 gcc mfetch.c -o mfetch.exe
+
 if %errorlevel% equ 0 (
     echo Build successful!
     copy mfetch.exe %USERPROFILE%\bin\ >nul 2>&1
