e1f6c946acb60eec5c9b090bd75e0a7fe4dcc7be
ymir <joshmanicom@gmail.com>
2026-01-14 11:19:53 -0800

    Modified batch command to copy to PATH to be able to run command with just mfetch

--- build.bat
+++ build.bat
@@ -2,6 +2,8 @@
 gcc mfetch.c -o mfetch.exe
 if %errorlevel% equ 0 (
     echo Build successful!
+    copy mfetch.exe %USERPROFILE%\bin\ >nul 2>&1
+    copy mfetch.exe %USERPROFILE%\msys64\ucrt64\bin >nul 2>&1
     mfetch.exe
 )   else (
     echo Build failed!
